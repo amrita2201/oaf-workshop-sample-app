6ccc7506a8a87193499c6d75c9195d39ccb36067
Fnu Mandakini <mandakinidsi@gmail.com>
2026-07-24 14:09:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Update package lists and install required Python packages
 sudo apt update -y
 sudo apt install -y python3-pip python3-flask gunicorn curl
@@ -7,11 +8,11 @@ sudo apt install -y python3-pip python3-flask gunicorn curl
 mkdir -p /home/ubuntu/app
 cd /home/ubuntu/app
 
-# Download app.py directly from the GitHub repository main branch
-curl -sSL https://raw.githubusercontent.com/Madhuparna04/oaf-workshop-sample-app/main/app.py -o app.py
+# Download app.py from your fork
+curl -sSL https://raw.githubusercontent.com/amrita2201/oaf-workshop-sample-app/main/app.py -o app.py
 
 # Ensure correct ownership
 chown -R ubuntu:ubuntu /home/ubuntu/app
 
-# Run app using Gunicorn on Port 80 (accessible by AWS ALB)
+# Run app using Gunicorn on port 80
 sudo gunicorn --bind 0.0.0.0:80 app:app --daemon
